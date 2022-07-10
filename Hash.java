b306f53ad0c94cc5a485ac68c431cc81ea67461f
Sanjithj <sanjithj591997@gmail.com>
2022-07-10 16:03:30 +0530

    Added: frequency check

--- Hash.java
+++ Hash.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 
-public class Hash {
+public class Hash<K> {
 	class Hashtable<K, V> {
 	    Node head;
 	    Node tail;
@@ -23,5 +23,56 @@ public class Hash {
 		}
 	}
 
+	private Object head;
+	private Node tail;
+	 //Append in linked list
+    public void append(Node myNode) {
+        if(this.head == null)
+            this.head = myNode;
+        if(this.tail == null)
+            this.tail = myNode;
+        else {
+            this.tail.setNext(myNode);
+            this.tail = myNode;
+        }
+    }
 
+    public Object getHead() {
+		return head;
+	}
+
+	public void setHead(Object head) {
+		this.head = head;
+	}
+
+	public Node getTail() {
+		return tail;
+	}
+
+	public void setTail(Node tail) {
+		this.tail = tail;
+	}
+
+	//Searching word in linked list
+    public Node<K, V> searchNode(K data) {
+        Node currentNode = (Node) head;
+        int position = 0;
+        while (currentNode != null) {
+            position++;
+            if (currentNode.getKey().equals(data)) {
+                return currentNode;
+            }
+            currentNode = currentNode.getNext();
+        }
+        return currentNode;
+    }
+
+    @Override
+    public String toString() {
+    return "LinkedListNodes{" + head + "}";
+    }
+
+    public void printNodes() {
+    System.out.println("Nodes: " + head);
+    }
 }
